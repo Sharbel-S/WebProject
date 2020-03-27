f3918f7d876a1b9324d254e79075d7ad4cb757cf
sharbel <sharbel.sekias@etu.univ-amu.fr>
2020-03-27 13:28:14 +0100

    Adding login and new user methodes

--- model.js
+++ model.js
@@ -3,3 +3,17 @@
 const Sqlite = require('better-sqlite3');
 
 let db = new Sqlite('db.sqlite');
+
+
+exports.login_student = (name, password) => {
+    var id = db.prepare('SELECT id FROM studentusers WHERE name=? AND password=?').get([name, password]);
+    console.log(name);
+    if (id == null) return -1;
+    return id;
+  }
+
+exports.new_student_user = function(name, password) {
+    var insertUser = db.prepare('INSERT INTO studentusers (name, password) VALUES (?, ?)');
+    var id = insertUser.run([name, password]).lastInsertRowid;
+    return id;
+  }
